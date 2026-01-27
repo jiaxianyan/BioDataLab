79bd6c2def75d50bc5fdd6a5d2d908be2917c7db
jiaxianyan <91183609+jiaxianyan@users.noreply.github.com>
2026-01-27 16:55:21 +0800

    new version

--- data/biodatalab_data/benchmark/eval_tools/eval_interface.py
+++ data/biodatalab_data/benchmark/eval_tools/eval_interface.py
@@ -1,21 +1,43 @@
-# import importlib
+from .common_eval import  *  
+from .specific_eval import *
 
-# def read_biodatalab_eval_module2api():
-#     fields = [
-#         "eval",
-#     ]
+eval_tool_dict = {
+                    'eval_str_from_file_equal':eval_str_from_file_equal,
+                    'eval_fasta_file_equal': eval_fasta_from_file_equal,
 
-#     module2api = {}
-#     for field in fields:
-#         module_name = f"data.biodatalab_data.benchmark.eval_tools.{field}"
-#         module = importlib.import_module(module_name)
-#         module2api[f"assistant.tool.{field}"] = module.description
-#     return module2api
+                    'eval_numeric_value_from_file_equal_round': eval_numeric_value_from_file_equal_round,
+                    'eval_numeric_value_list_from_file_equal_round': eval_numeric_value_list_from_file_equal_round,
 
-from .common_eval import eval_list
-from .AlphaFold_Protein_Structure_Database.eval import eval_AlphaFold_Protein_Structure_Database_fasta_file
-from .ASMdb.eval import eval_ASMdb_fastq
+                    'eval_str_list_from_json_equal_unsort': eval_str_list_from_json_equal_unsort,
+                    'eval_str_list_from_json_equal_sort': eval_str_list_from_json_equal_sort,
 
-eval_tool_dict = {'eval_list':eval_list,
-                  'eval_AlphaFold_Protein_Structure_Database_fasta_file':eval_AlphaFold_Protein_Structure_Database_fasta_file,
-                  'eval_ASMdb_fastq':eval_ASMdb_fastq}
+                    'eval_dict_from_json_equal': eval_dict_from_json_equal,
+
+                    'eval_csv_file_equal_sort': eval_csv_file_equal_sort,
+                    'eval_csv_file_equal_unsort': eval_csv_file_equal_unsort,
+
+                    'eval_sam_bam_equal': eval_sam_bam_flagstat,
+
+                    'eval_tsv_file_unsort': eval_tsv_file_unsort,
+                    'eval_tsv_file_sort': eval_tsv_file_sort,
+
+                    'eval_bed_file_equal': eval_bed_file_equal,
+
+                    'eval_vcf_file_equal': eval_vcf_file_equal,
+
+                    'eval_fastq_file_equal': eval_fastq_file_equal,
+
+                    'eval_vcf_gz_file_equal': eval_vcf_gz_file_equal,
+
+                    'eval_gtf_file_equal': eval_gtf_file_equal,
+
+                    'eval_fq_gz_file_equal': eval_fq_gz_file_equal,
+
+                    'eval_psl_file_equal': eval_psl_file_equal,
+
+                    'eval_rds_file_equal': eval_rds_file_equal,
+
+                    'eval_h5ad_file_equal': eval_h5ad_file_equal,
+
+                    'eval_specific_coding_10': eval_specific_coding_10
+                    }
